e900e9791edafa2a20ecae215252ef88bb2916e8
Justineo <justice360@gmail.com>
2014-11-02 04:22:54 +0800

    update style

--- fi.js
+++ fi.js
@@ -194,6 +194,7 @@
         'left: 0',
         'width: 100%',
         'height: 100%',
+        'overflow: auto',
         'background-color: rgba(0, 0, 0, 0.7)',
         'padding: 15px 20px',
         'line-height: 1.5',
