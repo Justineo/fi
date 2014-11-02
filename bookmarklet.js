e46db0bae987e1f9e080682745736c7b48925f31
Justineo <justice360@gmail.com>
2014-11-02 19:55:37 +0800

    revert to inline code to avoid being blocked by security policies

--- bookmarklet.js
+++
@@ -1,5 +0,0 @@
-(function () {
-  var script = document.createElement('script');
-  script.setAttribute('src', 'http://justineo.github.io/fi/fi.min.js');
-  document.body.appendChild(script);
-}());
--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,17 @@ var rename = require('gulp-rename');
 var mustache = require("gulp-mustache");
 var fs = require('fs');
 
-gulp.task('minify-fi', function() {
+gulp.task('minify', function() {
   return gulp.src('./fi.js')
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('minify-bookmarklet', function() {
-  return gulp.src('./bookmarklet.js')
-    .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('.'));
-});
-
-gulp.task('home', ['minify-fi', 'minify-bookmarklet'], function() {
+gulp.task('home', ['minify'], function() {
   return gulp.src("./index.tpl")
     .pipe(mustache({
-      code: fs.readFileSync('./bookmarklet.min.js', {
+      code: fs.readFileSync('./fi.min.js', {
           encoding: 'utf8'
       })
     }))
